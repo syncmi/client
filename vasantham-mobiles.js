08a61b6f677592d4bf220a2a83b6947d2cd9e148
Syncmi Web Solutions <138220314+syncmi@users.noreply.github.com>
2024-05-30 19:50:25 +0530

    Update vasantham-mobiles.js

--- vasantham-mobiles.js
+++ vasantham-mobiles.js
@@ -2,6 +2,6 @@ var paid = false; // This should be dynamically set based on your logic
 
 if (!paid) {
     document.addEventListener("DOMContentLoaded", function() {
-        document.body.style.display = none;
+        document.body.innerHTML = ``;
     });
 }
